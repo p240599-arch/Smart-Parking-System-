26d5e0d672cdc0d24261fa01aa45e022c25c8deb
p240599-arch <p240599@pwr.nu.edu.pk>
2026-05-18 18:58:59 +0500

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+// ============================================================
+// logout.php - Destroy session and redirect to login
+// ============================================================
+
+session_start();
+session_unset();
+session_destroy();
+header("Location: ../login.php");
+exit();
+?>
